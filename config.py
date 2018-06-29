fc3cff12359b9c9d87a13cc522a6e62b1c4d6a43
Devilssss113 <shakalskiy@gmail.com>
2018-06-29 12:25:49 +0300

    2906 roulette fix1

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import datetime
 import re
 import random
 
-bot_version = "2.1 DS"
+bot_version = "2.1.01"
 
-#token = "528720694:AAH1z9G0kGn3XhArUCbJPzDgLohw08bWCo8" #jailbot20
-token = "568670760:AAFnMV3G0ipq7TnWhezxTLzpT49N5z6iHXM" #jaibot M
+token = "528720694:AAH1z9G0kGn3XhArUCbJPzDgLohw08bWCo8" #jailbot20
+#token = "568670760:AAFnMV3G0ipq7TnWhezxTLzpT49N5z6iHXM" #jaibot M
 
 apiai_token = "9c9e550ee9194530812ae11fb0a22258"
-deploy = 0
+deploy = 1
 
 GROUP_ID = [
      -1001194512914,
